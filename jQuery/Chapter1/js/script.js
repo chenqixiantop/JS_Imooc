cab34e37d62c7f297a1b9ea15e2e5094b588d1d5
chenqixiantop <chenqixiantop@163.com>
2019-05-21 23:09:49 +0800

    jquery update

--- jQuery/Chapter1/js/script.js
+++ jQuery/Chapter1/js/script.js
@@ -1,7 +1,5 @@
-$(document).ready(function(){
-    $('a').click(function(){
-        $('img').eq($(this).index())
-        .css({'opacity':'1'})
-        .siblings().css({'opactity':'0'});
+$(document).ready(function() {
+    $('a').click(function() {
+        $('img').eq($(this).index()).css({ 'opacity': '1' }).siblings().css({ 'opacity': '0' });
     });
 });
