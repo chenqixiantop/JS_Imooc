586aade9a0287febbf73fff4c59c2ef0d9e113df
chenqixiantop <chenqixiantop@163.com>
2019-05-27 23:34:57 +0800

    给轮播图添加了事件

--- jQuery/Chapter1/js/script.js
+++ jQuery/Chapter1/js/script.js
@@ -1,5 +1,46 @@
 $(document).ready(function() {
-    $('a').click(function() {
-        $('img').eq($(this).index()).css({ 'opacity': '1' }).siblings().css({ 'opacity': '0' });
+    // $('a').click(function() {
+    //     $('img').eq($(this).index()).css({ 'opacity': '1' }).siblings().css({ 'opacity': '0' });
+    // });
+
+    //鼠标双击事件
+    // $('a').dblclick(function(){
+    //     $('img').eq($(this).index())
+    //     .css({'opacity':'1'})
+    //     .siblings().css({'opacity':'0'});
+    // });
+
+
+   //鼠标按下时触发
+   $('a').mousedown(function(){
+       console.log("鼠标按下图片"+$(this).index()+"了");
+   });
+   //按下鼠标放松时触发
+
+//    $('a').mouseup(function(){
+//         $('img').eq($(this).index()).css({'opacity':'1'})
+//           .siblings().css({'opacity':'0'});
+//    })
+
+
+    //鼠标进入
+    // $('a').mouseenter(function(){
+    //     $('img').eq($(this).index()).css({'opacity':'1'})
+    //       .siblings().css({'opacity':'0'});
+    // })
+    
+    // //鼠标移开
+    // $('a').mouseleave(function(){
+    //     console.log("鼠标移开"+$(this).index()+"图片了");
+    // });
+
+    //hover鼠标进入和退出
+    $('a').hover(function(){
+        $('img').eq($(this).index()).css({'opacity':'1'})
+          .siblings().css({'opacity':'0'});
+    },function(){
+        $('img').eq($(this).index()).css({'opacity':'0'})
+          .siblings().css({'opacity':'1'});
     });
 });
+
